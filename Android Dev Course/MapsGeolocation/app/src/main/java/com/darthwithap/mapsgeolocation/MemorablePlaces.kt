06af72eb66dcf1003b1597774fbf12bf2d72e563
Parth Takkar <parthtakkar@gmail.com>
2021-03-25 20:59:04 +0530

    initial hikers watch

---
+++ Android Dev Course/MapsGeolocation/app/src/main/java/com/darthwithap/mapsgeolocation/MemorablePlaces.kt
@@ -0,0 +1,11 @@
+package com.darthwithap.mapsgeolocation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemorablePlaces : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memorable_places)
+    }
+}
--- Android Dev Course/MapsGeolocation/app/src/main/java/com/darthwithap/mapsgeolocation/UsersLocation.kt
+++ Android Dev Course/MapsGeolocation/app/src/main/java/com/darthwithap/mapsgeolocation/UsersLocation.kt
@@ -3,53 +3,85 @@ package com.darthwithap.mapsgeolocation
 import android.content.Context
 import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.Task
+import java.util.*
 import java.util.jar.Manifest
 
 class UsersLocation : AppCompatActivity(), OnMapReadyCallback {
-    
+
     lateinit var fusedLocationProviderClient : FusedLocationProviderClient
     lateinit var locationRequest : LocationRequest
     lateinit var locationCallback : LocationCallback
+    private lateinit var mMap: GoogleMap
+    private lateinit var currLocation : Location
+    private lateinit var liveLocationButton : Button
+    private lateinit var userLocationButton : Button
+    private lateinit var mMarker: Marker
+    private val TAG = "UsersLocation"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_users_location)
 
+        liveLocationButton = findViewById<Button>(R.id.btn_user_live_location)
+        userLocationButton = findViewById<Button>(R.id.btn_user_location)
+
+        liveLocationButton.apply {
+            visibility = View.INVISIBLE
+            isClickable = false
+        }
+        userLocationButton.apply {
+            visibility = View.INVISIBLE
+            isClickable = false
+        }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        currLocation = Location("Dummy").apply {
+            longitude = 86.8869557
+            latitude = 28.0249447
+        }
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map_user) as SupportMapFragment
         mapFragment.getMapAsync(this@UsersLocation)
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
-        findViewById<Button>(R.id.btn_user_live_location).setOnClickListener {
-            locationCallback = object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult?) {
-                    locationResult ?: return
-                    for (location in locationResult.locations) {
-                        Toast.makeText(this@UsersLocation, "${location.longitude} and ${location.latitude}", Toast.LENGTH_SHORT)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    currLocation = location
+                    Toast.makeText(this@UsersLocation, "${location.longitude} and ${location.latitude}", Toast.LENGTH_SHORT)
                             .show()
-                    }
+                    updateMap(location)
                 }
             }
+        }
+
+        liveLocationButton.setOnClickListener {
             createLocationRequest()
         }
 
-        findViewById<Button>(R.id.btn_user_location).setOnClickListener {
+        userLocationButton.setOnClickListener {
             getLastLocation()
         }
 
@@ -118,14 +150,49 @@ class UsersLocation : AppCompatActivity(), OnMapReadyCallback {
             return
         }
         task.addOnSuccessListener {
+            currLocation = it
             if (it != null) {
                 Toast.makeText(applicationContext, "Last Location : ${it.latitude}, ${it.longitude}", Toast.LENGTH_SHORT).show()
+                updateMap(it)
+                val geocoder = Geocoder(applicationContext, Locale.getDefault())
+                try {
+                    val addresses = geocoder.getFromLocation(currLocation.latitude, currLocation.longitude, 1)
+
+                    if (addresses != null && addresses.size>0) {
+                        Log.d(TAG, "onLocationResult: Addresses: ${addresses[0].adminArea}")
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(googleMap : GoogleMap?) {
+        liveLocationButton.apply {
+            visibility = View.VISIBLE
+            isClickable = true
+        }
+        userLocationButton.apply {
+            visibility = View.VISIBLE
+            isClickable = true
+        }
 
+        if (googleMap != null) {
+            mMap = googleMap
+
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            val location = LatLng(currLocation.latitude, currLocation.longitude)
+            mMarker = mMap.addMarker(MarkerOptions().position(location).title("Your Current Location!")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f))
+        }
+    }
+
+    private fun updateMap(location: Location) {
+        mMarker.position = LatLng(location.latitude, location.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude)))
     }
 
 }
